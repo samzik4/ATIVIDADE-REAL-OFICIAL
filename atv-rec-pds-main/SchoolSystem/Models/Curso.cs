0084dc1b99e1fbbaf32507acc2e43dc30c13a51e
Samuel Felipe <samuel.felipe@estudante.ifro.edu.br>
2022-08-23 07:40:14 -0400

    inserção do code

---
+++ atv-rec-pds-main/SchoolSystem/Models/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CargaHoraria { get; set; }
+        public string Turno { get; set; }
+        public string Descricao { get; set; }
+
+        public Escola Escola { get; set; }
+    }
+}
